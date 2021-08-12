e2dca27467ff91cd26ae06e9dea0eaa9893f3d06
Gabriel7729 <Lusan7729@gmail.com>
2021-08-12 13:04:23 -0400

    Initial

---
+++ SignalR Challenge/Hubs/Todo_List_Hub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR_Challenge.Hubs
+{
+    public class Todo_List_Hub : Hub
+    {
+        public async Task SendElement(string todo)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", todo);
+        }
+    }
+}
---
+++ SignalR Challenge/wwwroot/js/todoList.js
@@ -0,0 +1,29 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/todoList").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (todo) {
+    var li = document.createElement("li");
+    document.getElementById("listTodo").appendChild(li);
+    // We can assign user-supplied strings to an element's textContent because it
+    // is not interpreted as markup. If you're assigning in any other way, you 
+    // should be aware of possible script injection concerns.
+    li.textContent = `${todo}`;
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var todo = document.getElementById("todoInput").value;
+    connection.invoke("SendElement", todo).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
